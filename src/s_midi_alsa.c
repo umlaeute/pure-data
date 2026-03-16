aea84969285501ff1db06a780355f8f5bb7f0e63
IOhannes m zmölnig <zmoelnig@iem.at>
2026-03-16 13:15:47 +0100

    ALSA-MIDI: always free MIDI event handler (if it exists)

--- src/s_midi_alsa.c
+++ src/s_midi_alsa.c
@@ -250,11 +250,11 @@ void sys_alsa_close_midi()
     {
         snd_seq_close(midi_handle);
         midi_handle = NULL;
-        if (midiev)
-        {
-            snd_midi_event_free(midiev);
-            midiev = NULL;
-        }
+    }
+    if (midiev)
+    {
+        snd_midi_event_free(midiev);
+        midiev = NULL;
     }
 }
 
